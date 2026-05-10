673c9161fc3e057f0ebf491c44fa751abf66bb4e
ElizaAgora <pokormianona@gmail.com>
2026-05-10 23:07:23 +0300

    update sw

--- sw.js
+++ sw.js
@@ -1,35 +1,19 @@
-const CACHE_NAME = 'meal-planner-v3';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/manifest.json',
-  '/js/app.js',
-  '/js/database.js',
-  '/js/products/products.js',
-  '/js/menu/menu.js',
-  '/js/menu/menu-components.js',
-  '/js/menu/menu-ratings.js',
-  '/js/menu/menu-subtract.js',
-  '/js/stats/stats.js',
-  '/js/shopping/shopping.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
-];
+const CACHE_NAME = 'meal-planner-v4';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Кеширование файлов');
-      return cache.addAll(urlsToCache);
-    })
-  );
+    console.log('SW: install');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('SW: activate');
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
